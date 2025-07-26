4d81dd040385cf9e312232dbd9925e0797cf4b55
“Khshanovskyi” <pavlo_khshanovskyi@epam.com>
2025-07-26 17:12:35 +0300

    Initial commit

--- mcp_server/tools/web_search.py
+++ mcp_server/tools/web_search.py
@@ -30,7 +30,7 @@ class WebSearchTool(BaseTool):
 
     def __init__(self, api_key: str, endpoint: str) -> None:
         self.api_key = api_key
-        self.endpoint = endpoint + "/openai/deployments/gemini-2.0-flash-exp-google-search/chat/completions"
+        self.endpoint = endpoint + "/openai/deployments/gemini-2.5-pro/chat/completions"
 
     def execute(self, arguments: dict[str, Any]) -> str:
         headers = {
@@ -43,7 +43,18 @@ class WebSearchTool(BaseTool):
                     "role": "user",
                     "content": str(arguments["request"])
                 }
-            ]
+            ],
+            "tools": [
+                {
+                    "type": "static_function",
+                    "static_function": {
+                        "name": "google_search",
+                        "description": "Grounding with Google Search",
+                        "configuration": {}
+                    }
+                }
+            ],
+            "temperature": 0
         }
 
         response = requests.post(url=self.endpoint, headers=headers, json=request_data)
